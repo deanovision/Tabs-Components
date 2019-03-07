dda1a0a3e173dc2acfc85efbc6333bc7ce798a89
deanovision <rodeanfraser@gmail.com>
2019-03-07 16:12:05 -0500

    started tab navigation component, completed loop and now setting up constructor class

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,19 +2,19 @@
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
-    // this.element;
+    this.element = element;
     
     // Get the custom data attribute on the Link
-    // this.data;
+    this.data = this.element.querySelector(".tabs-link");
     
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
+    this.itemElement = document.querySelector(`.tab-item[data-tab="${this.element.dataset.tab}"]`);
     
     // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem;
+    this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
-
+    this.element.addEventListener("click", () => select());
   };
 
   select() {
@@ -59,4 +59,6 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+links = document.querySelectorAll(".tabs-link").forEach(function(tabLinkItem){
+  return new TabLink(tabLinkItem);
+});
